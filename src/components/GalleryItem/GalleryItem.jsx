69caf2a9d75c7ae83355d66a12a56324b7b5aedd
AlinaTrukhina <alina.trukhina@gmail.com>
2022-10-21 18:15:31 -0500

    like button in item file

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,30 @@
+import axios from 'axios';
+
 function GalleryItem({photo}) {
+
+    function handleLike() {
+        console.log(photo.id);
+        likePhoto(photo);
+    }
+
+    function likePhoto(photo) {
+        axios({
+            method: 'PUT',
+            url: `/gallery/like/${photo.id}`
+          })
+          .then(response=>{
+            console.log('PUT done');
+          })
+          .catch((error)=>{
+            console.log('error in PUT endpoint', error);
+          })
+    }
+
     return (
         <>
         <img src={photo.path}/>
         <figcaption>{photo.description}</figcaption>
+        <button onClick={handleLike}>Like</button>
         </>
     );
 }
