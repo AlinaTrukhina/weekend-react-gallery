38180da17fa664025854dda2e44efc1e073286d1
AlinaTrukhina <alina.trukhina@gmail.com>
2023-02-14 17:05:41 -0600

    click on photo test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
+
+    cy.contains('Gallery')
+
+    cy.get('ul li:first > img').click().get('.photoDescription')
+
+    cy.get('.photoDescription').contains('morning')
   })
 })
 
